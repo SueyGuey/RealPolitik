b7d2641163cafdbf05130aa1adad64eb608cca56
Suhan Gui <suhangui6@gmail.com>
2020-09-22 02:37:08 -0400

    final req.txt 9/22

--- src/realpolitik/settings.py
+++ src/realpolitik/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import heroku_django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,3 +147,4 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PW')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+heroku_django.settings(locals())
