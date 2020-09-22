d388f3b69558cf762482d1c32d5d177631e28855
Suhan Gui <suhangui6@gmail.com>
2020-09-22 01:58:08 -0400

    addings environ vars 9/22

--- src/realpolitik/settings.py
+++ src/realpolitik/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5-!4*i8f=6y8ed_!yp&kicg4c3tv^$6x*h=r#b02etdje*(2@%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://therealpolitik.herokuapp.com']
 
 
 # Application definition
