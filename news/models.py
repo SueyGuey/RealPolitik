4ef500b19713d11102aeacb1d65db300f40724df
Suhan Gui <suhangui6@gmail.com>
2020-09-22 23:05:10 -0400

    fixed email recovery

--- news/models.py
+++ news/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 class Article(models.Model):
 	title = models.TextField()
 	author = models.TextField(default = "Author")
-	image_url = models.TextField(null = True, blank = True)
-	url = models.URLField()
+	image_url = models.URLField(null = True, blank = True)
+	url = models.TextField()
 	site = models.TextField(max_length = 99,default = "News")
 	site_url = models.URLField(default = "google.com")
 
--- realpolitik/settings.py
+++ realpolitik/settings.py
@@ -139,10 +139,18 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PW')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'theofficialsuhan@gmail.com'
+"""
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER') #my personal info
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PW')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PW')"""
 
 django_heroku.settings(locals())
